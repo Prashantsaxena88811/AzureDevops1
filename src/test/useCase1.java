53e00f1642b2e185a2cc62cfce927e3bb39385ea
Prashant saxena <Prashantsxn8@gmail.com>
2020-07-07 15:17:12 +0530

    aaaaaa4

--- src/test/useCase1.java
+++ src/test/useCase1.java
@@ -11,6 +11,7 @@ public class UseCase1 {
 		// TODO` Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
 		driver.get("https://login.salesforce.com/");
 		driver.findElement(By.xpath("//input[@class='input r4 wide mb16 mt8 username']")).sendKeys("Prashantsxntesting8@gmail.com");
 		driver.findElement(By.xpath("//input[@class='input r4 wide mb16 mt8 password']")).sendKeys("Prashant@123");
